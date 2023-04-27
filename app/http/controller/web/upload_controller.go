0ef2db075f56d0cd72fba13414558c3c9b8f7510
aloys <aloys.zy@hotmail.com>
2023-04-27 14:27:44 +0800

    修改上下文的字段名称

--- app/http/controller/web/upload_controller.go
+++ app/http/controller/web/upload_controller.go
@@ -5,20 +5,20 @@ import (
 	"ginskeleton/app/global/variable"
 	"ginskeleton/app/service/upload_file"
 	"ginskeleton/app/utils/response"
+
 	"github.com/gin-gonic/gin"
 )
 
-type Upload struct {
-}
+type Upload struct{}
 
 //	文件上传是一个独立模块，给任何业务返回文件上传后的存储路径即可。
 //
 // 开始上传
-func (u *Upload) StartUpload(context *gin.Context) {
+func (u *Upload) StartUpload(ctx *gin.Context) {
 	savePath := variable.BasePath + variable.ConfigYml.GetString("FileUploadSetting.UploadFileSavePath")
-	if r, finnalSavePath := upload_file.Upload(context, savePath); r == true {
-		response.Success(context, consts.CurdStatusOkMsg, finnalSavePath)
+	if r, finnalSavePath := upload_file.Upload(ctx, savePath); r == true {
+		response.Success(ctx, consts.CurdStatusOkMsg, finnalSavePath)
 	} else {
-		response.Fail(context, consts.FilesUploadFailCode, consts.FilesUploadFailMsg, "")
+		response.Fail(ctx, consts.FilesUploadFailCode, consts.FilesUploadFailMsg, "")
 	}
 }
